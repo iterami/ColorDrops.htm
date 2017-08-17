56d81dd03113875c15ada6cd3e5501827bec5c7f
honzi <czechinthemail@gmail.com>
2017-08-17 13:15:28 -0700

    added canvas_setproperties() usage

--- js/colordrops.js
+++ js/colordrops.js
@@ -7,7 +7,11 @@ function draw_logic(){
         'canvas',
       ],
       'todo': function(entity){
-          canvas_buffer.fillStyle = core_entities[entity]['color'];
+          canvas_setproperties({
+            'properties': {
+              'fillStyle': core_entities[entity]['color'],
+            },
+          });
           canvas_buffer.fillRect(
             core_entities[entity]['x'] - core_entities[entity]['width'],
             core_entities[entity]['y'] - core_entities[entity]['height'],
