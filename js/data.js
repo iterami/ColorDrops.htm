dbe0971660645d75042875e9eb8f8bc10a46ccef
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 12:53:48 -0700

    combined js into repo.js

--- js/data.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-function load_data(id){
-    core_mouse['down-x'] = canvas_properties['width-half'];
-    core_mouse['down-y'] = canvas_properties['height-half'];
-}
--- repo.js
+++ repo.js
@@ -1,5 +1,10 @@
 'use strict';
 
+function load_data(id){
+    core_mouse['down-x'] = canvas_properties['width-half'];
+    core_mouse['down-y'] = canvas_properties['height-half'];
+}
+
 function repo_drawlogic(){
     entity_group_modify({
       'groups': [
